1d9862e9db5471195b662000da14b74fe15e990b
Can Löfstrand <Can.Lofstrand@yaelev.se>
2019-10-03 14:37:25 +0200

    Add project files.

---
+++ AuthDemo/App_Start/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using AuthDemo.Models;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(AuthDemo.App_Start.Startup))]
+
+namespace AuthDemo.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/Token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new AuthValidationProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
---
+++ AuthDemo/Models/UserMasterRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthDemo.Models
+{
+    public class UserMasterRepository : IDisposable
+    {
+         BookingEntities1 context = new BookingEntities1();
+
+        public UserMaster ValidateUser(string username, string password)
+        {
+            return context.UserMaster.FirstOrDefault(user => user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+            && user.UserPassword == password);
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
